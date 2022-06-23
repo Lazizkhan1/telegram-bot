cb8881495d524c6364675063d398ebd0a1a32578
lazizkhan1 <lazizkhanhadjayev@gmail.com>
2022-06-23 23:28:27 +0500

    defined parameter and return value type

--- currency_module.py
+++ currency_module.py
@@ -5,7 +5,8 @@ response_all = requests.get("https://nbu.uz/uz/exchange-rates/json/").text
 
 response = json.loads(response_all)
 
-def get_currency(currency):
+
+def get_currency(currency: str) -> dict:
     for i in response:
         if i['code'] == currency:
             return i
