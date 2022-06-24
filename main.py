4477e2693c9fa2728f330ea4c5e281f7bb4cdc76
lazizkhan1 <lazizkhanhadjayev@gmail.com>
2022-06-24 23:26:06 +0500

    added forward message

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import telebot
+from private_variables import api_key
 from currency_module import get_currency
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-file = open("bot-token.txt")
-api_key = file.read()
-file.close()
-
 
 bot = telebot.TeleBot(api_key, parse_mode=None)
 
@@ -18,10 +14,15 @@ bot.set_my_commands([
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
+    bot.reply_to(message, "Qelesiz endi ")
+
+
+@bot.message_handler(commands=['photo'])
+def send_welcome(message):
     bot.reply_to(message, "Qelsz endi ")
 
 
-@bot.message_handler(commands=['creator', 'admin'])
+@bot.message_handler(commands=['creator'])
 def send_creator_id(message):
     bot.reply_to(message, "@lazizkhan1")
 
@@ -32,20 +33,21 @@ def send_currency(message):
     item_us = InlineKeyboardButton(text="Aqsh Dollar", callback_data='USD')
     item_ru = InlineKeyboardButton(text="Rossiya Rubl", callback_data='RUB')
     markup_inline.add(item_us, item_ru)
-    bot.send_message(message.chat.id, "Valyutani tanlang", reply_markup= markup_inline)
+    bot.send_message(message.chat.id, "Valyutani tanlang", reply_markup=markup_inline)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def send_currency(call):
     currency = get_currency(call.data)
-    bot.send_message(call.message.chat.id, f"Bugungi sana: {currency['date']} \n\n{currency['title']}: {currency['cb_price']} so'm\n")
+    bot.send_message(call.message.chat.id,
+                     f"Bugungi sana: {currency['date']} \n\n{currency['title']}: {currency['cb_price']} so'm\n")
+
 
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
+    # bot.forward_message(968242298, message.from_user.id, message.id) # you can forward users message to another chat or user
     bot.reply_to(message, message.text)
 
 
 bot.infinity_polling()
-
-
